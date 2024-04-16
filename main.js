0d6e2e03c75438290d1d5f441165b549c13c94c1
n.suzuki@dual.tokyo <n.suzuki@dual.tokyo>
2024-04-16 19:00:15 +0900

    テーブルのフォームを1行追加する機能の実装

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <div>
+//テーブルフォームを1行追加する
+document.addEventListener("DOMContentLoaded", function () {
+  const addButton = document.getElementById("addTableButton");
+  addButton.addEventListener("click", addTableForm);
+});
 
-  </div>
-`
-
+function addTableForm() {
+  const tableForm = document.createElement("div");
+  tableForm.classList.add("tableForm"); 
+  tableForm.innerHTML = `
+    <label for="numTables">テーブル：</label>
+    <input type="number" class="seatsPerTable" min="1">人掛けのテーブルが
+    <input type="number" class="numTables" min="1">個
+  `;
+  const tableFormsContainer = document.getElementById("tableForms");
+  tableFormsContainer.appendChild(tableForm); 
+}
