4f84436b133a8fff8a958097b6d4239f95312c51
hikachiba <h.chiba@dual.tokyo>
2024-04-23 10:00:28 +0900

    特定の人の席を固定する機能を実装

--- main.js
+++ main.js
@@ -64,17 +64,21 @@ function checkParticipants(checkbox) {
 //参加者のみを条件のプルダウンリストに表示する
 const setParticipantsBtn = document.querySelector('#setParticipantsBtn');
 const distance = document.querySelector('#distance');
+const fixed = document.querySelector('#fixed');
 
-setParticipantsBtn.addEventListener('click', showMemberList);
+setParticipantsBtn.addEventListener('click', function() {
+  showMemberList(distance);
+  showMemberList(fixed);
+});
 
-function showMemberList() {
+function showMemberList(query) {
   let pulldown = '';
   let checked = document.querySelectorAll('.checked');
   checked.forEach(function(member) {
     pulldown += '<option value="' + member.value + '">' + employeeData[member.value] + '</option>';
   });
 
-  const newMemberList = document.querySelector('#distance').lastElementChild.querySelectorAll('.memberList');
+  const newMemberList = query.lastElementChild.querySelectorAll('.memberList');
   newMemberList.forEach(function(element) {
     element.innerHTML = '<option value="null"></option>' + pulldown;
   });
@@ -88,11 +92,18 @@ function showMemberList() {
 //   }
 // })
 
-//＋ボタンで次の行を追加
+//＋ボタンで「特定の人同士を近づける」の次の行を追加
 const addDistanceBtn = document.querySelector('#addDistanceBtn');
 addDistanceBtn.addEventListener('click', function(){
   distance.insertAdjacentHTML('beforeend', '<div><select class="memberList person1"></select>と<select class="memberList person2"></select>を<select id="arrangement"><option value="null"></option><option value="">隣同士にする</option><option value="">同じテーブルにする</option></select></div>');
-  showMemberList();
+  showMemberList(distance);
+});
+
+//＋ボタンで「特定の人を固定する」の次の行を追加
+const addFixedBtn = document.querySelector('#addFixedBtn');
+addFixedBtn.addEventListener('click', function(){
+  fixed.insertAdjacentHTML('beforeend', '<div><select class="memberList fixedParticipant"></select>を<input type="number" class="fixedTable" min="1">番目のテーブルの<input type="number" class="fixedSeat" min="1">番の席に固定する</div>');
+  showMemberList(fixed);
 });
 
 //テーブルフォームを1行追加する
@@ -144,18 +155,22 @@ function showTables() {
   }
   
   document.querySelector('#result').innerHTML = result;
-  showRandomParticipant();
+  showRandomParticipants(numTablesList);
 }
 
 //参加者をランダムに配置する
-function showRandomParticipant() {
+function showRandomParticipants(numTablesList) {
+  //参加者を取得し、配列に格納する
   let participants = document.querySelectorAll('.checked');
   let participantsId = [];
   for(let i = 0; i < participants.length; i++){
     participantsId.push(participants[i].value);
   }
   shuffleArray(participantsId);
+  //console.log(participantsId);
+  fixParticipants(participantsId, numTablesList);
 
+  //席に配置する
   let tables = document.querySelectorAll('.table');
   if(tables.length < participantsId.length){
     alert('座席の数が足りません。');
@@ -166,10 +181,44 @@ function showRandomParticipant() {
   }
 }
 
+//配列をランダムな順番に並び替える
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
   }
   return array;
+}
+
+//特定の人の席を固定する
+function fixParticipants(array, numTablesList) {
+  let fixedParticipants = document.querySelectorAll('.fixedParticipant');
+  let fixedTables = document.querySelectorAll('.fixedTable');
+  let fixedSeats = document.querySelectorAll('.fixedSeat');
+  let fixedList = []; 
+  for(let i = 0; i < fixedParticipants.length; i++){
+    fixedList.push([fixedParticipants[i].value, fixedTables[i].value, fixedSeats[i].value]);
+  }
+
+  //座席数を前のテーブルから順に格納
+  let seatsList = [];
+  for(let i = 0; i < numTablesList.length; i++){
+    for(let j = 0; j < numTablesList[i][1]; j++){
+      seatsList.push(Number(numTablesList[i][0]));
+    }
+  }
+
+  for(let i = 0; i < fixedList.length; i++){
+    let participantIndex = array.indexOf(fixedList[i][0]);
+    let sum = 0;
+    for(let j = 0; j < Number(fixedList[i][1]) - 1; j++){
+      sum += seatsList[j];
+    }
+
+    let seatIndex = sum + Number(fixedList[i][2]) - 1;
+    let temp = array[participantIndex];
+    array[participantIndex] = array[seatIndex];
+    array[seatIndex] = temp;
+  }
+  //console.log(array);
 }
